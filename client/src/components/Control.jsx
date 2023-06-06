d1238038fb2f2e4be94dcda228fcd11c1090f786
KenWong <wongkenq@gmail.com>
2023-06-06 09:49:44 -0700

    added controller page

---
+++ client/src/components/Control.jsx
@@ -0,0 +1,63 @@
+import { Button, Box, Text, Flex, Select, Container } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { CiPlay1, CiPause1 } from 'react-icons/ci';
+import { GrPowerReset } from 'react-icons/gr';
+import { RxReset } from 'react-icons/rx';
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:3001');
+
+const Control = () => {
+  const [clockIsRunning, setClockIsRunning] = useState(false);
+
+  function handleModeSelect(value) {
+    socket.emit('send_mode', { value });
+  }
+
+  function handleClockIsRunning(value) {
+    setClockIsRunning(value);
+    socket.emit('send_clockIsRunning', { value });
+  }
+
+  function handleResetTimer() {
+    socket.emit('send_resetTimer', true);
+    console.log('reset');
+  }
+
+  return (
+    <Container mt="2rem">
+      <Box>
+        <Flex direction="column" justifyContent="center" alignItems="center" gap="1rem">
+          <Select
+            onChange={(e) => handleModeSelect(e.target.value)}
+            textAlign="center"
+            maxWidth={'75%'}
+            height="4rem"
+            fontSize="2rem"
+          >
+            <option value="randori">Randori</option>
+            <option value="uchikomi">Uchikomi</option>
+            <option value="threePerson">Three-Person</option>
+            <option value="waterBreak">Water Break</option>
+          </Select>
+          <Button
+            width="75%"
+            height="25vh"
+            fontSize="5rem"
+            onClick={
+              clockIsRunning ? () => handleClockIsRunning(false) : () => handleClockIsRunning(true)
+            }
+          >
+            {clockIsRunning ? <CiPause1 /> : <CiPlay1 />}
+          </Button>
+
+          <Button width="75%" height="10vh" fontSize="2rem" onClick={handleResetTimer}>
+            <RxReset />
+          </Button>
+        </Flex>
+      </Box>
+    </Container>
+  );
+};
+
+export default Control;
